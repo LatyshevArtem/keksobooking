703b88d59dba758c8e934210caaea4a9227e5ae9
LatyshevArtem <artem.l.2002@mail.ru>
2023-02-08 21:15:19 +1000

    Added the function to change the address in the form

--- js/ad-form.js
+++ js/ad-form.js
@@ -3,6 +3,7 @@ const selectType = document.querySelector('#type');
 const inputPrice = document.querySelector('#price');
 const selectTimein = document.querySelector('#timein');
 const selectTimeout = document.querySelector('#timeout');
+const address = document.querySelector('#address');
 
 const MIN_PRICE_BY_TYPE = {
   'bungalow': 0,
@@ -30,4 +31,10 @@ selectTimeout.addEventListener('change', () => {
   selectTimein.selectedIndex = selectTimeout.selectedIndex;
 })
 
-export { adForm, AD_FORM_DISABLED_CLASS_NAME };
+const setAddress = newAddress => {
+  const formatedLat = parseFloat(newAddress.lat).toFixed(5);
+  const formatedLng = parseFloat(newAddress.lng).toFixed(5);
+  address.value = `${formatedLat}, ${formatedLng}`
+}
+
+export { adForm, AD_FORM_DISABLED_CLASS_NAME, setAddress };
