3f3f048104c6d04fd14399d98fee501a742cb0c2
LatyshevArtem <artem.l.2002@mail.ru>
2023-02-09 19:58:52 +1000

    Added a callback function on successful data acquisition and on data receipt error

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 import { enableInactivePageState } from './page-state.js';
-import { loadMap } from './map.js';
+import { addSimilarAdPinMarker, loadMap } from './map.js';
 import { formInitialization } from './ad-form.js';
+import { METHODS, URLS, request } from './api.js';
+
+const onGetSimilarAdsSuccess = similarAds => {
+  similarAds.forEach(similarAd => addSimilarAdPinMarker(similarAd));
+}
+
+const onGetSimilarAdsError = error => {
+  alert(error);
+}
 
 enableInactivePageState();
 loadMap();
+request(onGetSimilarAdsSuccess, onGetSimilarAdsError, METHODS.GET, URLS.GET);
 formInitialization();
