06d821cbf46ffe7ddaa2f7b67a15077b3d49f12e
LatyshevArtem <artem.l.2002@mail.ru>
2023-03-31 19:33:54 +1000

    Refactored the code in the map-filters-form.js file and renamed it filters-form.js

---
+++ js/filters-form.js
@@ -0,0 +1,21 @@
+const FILTERS_FORM_DISABLED_CLASS_NAME = 'map__filters--disabled';
+
+const filtersForm = document.querySelector('.map__filters');
+
+const resetFiltersForm = () => filtersForm.reset();
+
+const lockFiltersForm = () => {
+  filtersForm.classList.toggle(FILTERS_FORM_DISABLED_CLASS_NAME);
+  for (let element of filtersForm.children) {
+    element.disabled = true;
+  }
+}
+
+const unlockFiltersForm = () => {
+  filtersForm.classList.toggle(FILTERS_FORM_DISABLED_CLASS_NAME);
+  for (let element of filtersForm.children) {
+    element.disabled = false
+  }
+}
+
+export { resetFiltersForm, lockFiltersForm, unlockFiltersForm };
--- js/map-filters-form.js
+++
@@ -1,7 +0,0 @@
-const mapFiltersForm = document.querySelector('.map__filters');
-
-const MAP_FILTERS_FORM_DISABLED_CLASS_NAME = 'map__filters--disabled';
-
-const resetMapFiltersForm = () => mapFiltersForm.reset();
-
-export { mapFiltersForm, MAP_FILTERS_FORM_DISABLED_CLASS_NAME, resetMapFiltersForm };
