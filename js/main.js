a039a8fab688067e8f702845d7445f088b552ea7
LatyshevArtem <artem.l.2002@mail.ru>
2023-04-01 12:27:40 +1000

    Removed old code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 import { enableInactivePageState } from './page-state.js';
 import { loadMap } from './map.js';
 import { formInitialization } from './ad-form.js';
-import { fetchAds } from './fetch-ads.js';
 
 enableInactivePageState();
 loadMap();
 formInitialization();
-fetchAds()
